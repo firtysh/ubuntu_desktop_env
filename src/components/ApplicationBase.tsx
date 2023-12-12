ebb3bbca6bfba62d6f12f024cf669b0f2748c5a1
Suman Mandal <sm2450@it.jgec.ac.in>
2023-12-12 23:44:46 +0530

    Add resize handler div in application base component

--- src/components/ApplicationBase.tsx
+++ src/components/ApplicationBase.tsx
@@ -15,8 +15,16 @@ const ApplicationBase = ({ applicationTitle, applicationBody, optionalHeader, de
         console.log('clicked on a application', e.target);
       }}
       ref={boxRef}
-      className=" flex flex-col min-w-[350px] w-[850px] min-h-[300px] h-[700px] bg-[rgba(0,0,0,0.1)] backdrop-blur absolute top-8 left-8 rounded-lg overflow-hidden shadow-inner shadow-white"
+      className="flex flex-col min-w-[350px] w-[850px] min-h-[300px] h-[700px] bg-[rgba(0,0,0,0.1)] backdrop-blur relative top-8 left-8 rounded-lg overflow-hidden shadow-inner shadow-white"
     >
+      <div className="absolute left-0 w-1 h-full cursor-w-resize leftHandler"></div>
+      <div className="absolute right-0 w-1 h-full cursor-e-resize rightHandler"></div>
+      <div className="absolute top-0 w-full h-1 cursor-n-resize topHandler"></div>
+      <div className="absolute bottom-0 w-full h-1 cursor-s-resize bottomHandler"></div>
+      <div className="absolute top-0 left-0 w-2 h-2 cursor-nw-resize topLeftHandler"></div>
+      <div className="absolute top-0 right-0 w-2 h-2 cursor-ne-resize topRightHandler"></div>
+      <div className="absolute bottom-0 left-0 w-2 h-2 cursor-sw-resize bottomLeftHandler"></div>
+      <div className="absolute bottom-0 right-0 w-2 h-2 cursor-se-resize bottomRightHandler"></div>
       <AppBar optional={optionalHeader} title={applicationTitle} parentRef={boxRef} desktopRef={desktopRef} />
       {applicationBody}
     </div>
